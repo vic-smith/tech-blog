1c28b7074b439bb793250ff735f05c74033f5d85
Victor Smith <victorj500@gmail.com>
2021-12-05 16:49:00 -0600

    seeded the tables

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,125 +2,29 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Donec posuere metus vitae ipsum.',
-    context: 'Post content',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
-    user_id: 10
-  },
-  {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    context: 'Post content',
-    post_url: 'https://nasa.gov/donec.json',
-    user_id: 8
-  },
-  {
-    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    context: 'Post content',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    title: 'Is JavaScript the best programming language?',
+    post_url: "https://www.hackreactor.com/blog/why-javascript-is-the-best-coding-language-to-learn",
+    context: 'JavaScript is the most popular language, and is still increasing in popularity.',
     user_id: 1
   },
   {
-    title: 'Nunc purus.',
-    context: 'Post content',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
-    user_id: 4
+    title: 'Most popular npm packages 2021.',
+    post_url: "",
+    context: 'Express, socket.io, async, rxjs, lodash, underscore, ramda.',
+    user_id: 2
   },
   {
-    title: 'Pellentesque eget nunc.',
-    context: 'Post content',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
-    user_id: 7
-  },
-  {
-    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    context: 'Post content',
-    post_url: 'https://stanford.edu/consequat.png',
-    user_id: 4
-  },
-  {
-    title: 'In hac habitasse platea dictumst.',
-    context: 'Post content',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    title: 'Raspberry Pi vs Arduino.',
+    post_url: "https://www.tomshardware.com/features/raspberry-pi-vs-arduino",
+    context: 'The Arduino is a truly versatile board but the Raspberry Pi is a full computer and a hardware hacking platform.',
     user_id: 1
   },
   {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    context: 'Post content',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
-    user_id: 1
-  },
-  {
-    title: 'Duis ac nibh.',
-    context: 'Post content',
-    post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
-    user_id: 9
-  },
-  {
-    title: 'Curabitur at ipsum ac tellus semper interdum.',
-    context: 'Post content',
-    post_url: 'https://reverbnation.com/ligula/sit.jpg',
-    user_id: 5
-  },
-  {
-    title: 'In hac habitasse platea dictumst.',
-    context: 'Post content',
-    post_url: 'http://china.com.cn/lectus/vestibulum.json',
+    title: 'Essential vs code extensions.',
+    post_url: "",
+    context: 'Settings Sync, Live Server, Remote-SSH.',
     user_id: 3
   },
-  {
-    title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    context: 'Post content',
-    post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
-    user_id: 10
-  },
-  {
-    title: 'Donec dapibus.',
-    context: 'Post content',
-    post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
-    user_id: 8
-  },
-  {
-    title: 'Nulla tellus.',
-    context: 'Post content',
-    post_url: 'https://lycos.com/natoque/penatibus/et.html',
-    user_id: 3
-  },
-  {
-    title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-    context: 'Post content',
-    post_url: 'https://gmpg.org/lorem.jpg',
-    user_id: 3
-  },
-  {
-    title:'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-    context: 'Post content',
-    post_url: 'https://paginegialle.it/mattis/egestas.jsp',
-    user_id: 7
-  },
-  {
-    title: 'In hac habitasse platea dictumst.',
-    context: 'Post content',
-    post_url: 'http://wikia.com/turpis/eget.jpg',
-    user_id: 6
-  },
-  {
-    title: 'Etiam justo.',
-    context: 'Post content',
-    post_url: 'https://shareasale.com/quis.json',
-    user_id: 4
-  },
-  {
-    title: 'Nulla ut erat id mauris vulputate elementum.',
-    context: 'Post content',
-    post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
-    user_id: 6
-  },
-  {
-    title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    context: 'Post content',
-    post_url: 'https://java.com/at/nibh/in.png',
-    user_id: 7
-  }
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
